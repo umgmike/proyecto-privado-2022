881d643aedd48fc754dbdb71bad58b01b580c617
umgmike <umgmike123@gmail.com>
2022-10-10 21:38:40 -0600

    cambio solo para administradores

--- resources/views/theme/pages/mantenimiento/roles/index.blade.php
+++ resources/views/theme/pages/mantenimiento/roles/index.blade.php
@@ -32,11 +32,13 @@
               </div>
             </div><br>
 
-            <div class="card-header">
-                <a href=" {{ route('page.create.roles') }} " class="btn btn-info tooltipsC" title="Crear registro del @yield('Title')">
-                  <i class="fa fa-fw fa-plus-circle"></i> Crear registro del @yield('Title')
-                </a>
-              </div>
+            @if ((Auth::user()->id_rol == 1) && (Auth::user()->condicion == 1))
+                <div class="card-header">
+                    <a href=" {{ route('page.create.roles') }} " class="btn btn-info tooltipsC" title="Crear registro del @yield('Title')">
+                        <i class="fa fa-fw fa-plus-circle"></i> Crear registro del @yield('Title')
+                    </a>
+                </div>
+            @endif
 
             <div class="card-body">
               <table id="example1" class="table table-bordered table-striped">
@@ -67,27 +69,33 @@
                         <td>{{ $rs->created_at }}</td>
                         <td>{{ $rs->updated_at }}</td>
                         <td>
-                            @if($rs->estado == 1)
-                                <a href="{{ route('page.edit.roles', ['id' => $rs->id])}}"´class=" tooltipsC" title="Editar registro">
-                                <i class="fa fa-edit btn btn-outline-info btn-xs"></i>
-                                </a>
-                            @endif
+                            @if ((Auth::user()->id_rol == 1) && (Auth::user()->condicion == 1))
+                                @if($rs->estado == 1)
+                                    <a href="{{ route('page.edit.roles', ['id' => $rs->id])}}"´class=" tooltipsC" title="Editar registro">
+                                    <i class="fa fa-edit btn btn-outline-info btn-xs"></i>
+                                    </a>
+                                @endif
 
-                            @if($rs->estado == 1)
-                                <form action="{{route('page.desactivar.Rol', ['id' => $rs->id])}}" class="d-inline form-eliminar" method="POST">
-                                @csrf @method("delete")
-                                <button type="submit" class="btn btn-outline-danger btn-xs btn-accion-tabla eliminar tooltipsC" title="Desactivar registro">
-                                    <i class="fa fa-trash"></i>
-                                </button>
-                                </form>
-
-                            @else
-                                <form action="{{route('page.desactivar.Rol', ['id' => $rs->id])}}" class="d-inline form-eliminar" method="POST">
-                                @csrf @method("delete")
-                                <button type="submit" class="btn btn-outline-success btn-xs btn-accion-tabla eliminar tooltipsC" title="Activar registro">
-                                    <i class="fa fa-recycle"></i>
+                                @if($rs->estado == 1)
+                                    <form action="{{route('page.desactivar.Rol', ['id' => $rs->id])}}" class="d-inline form-eliminar" method="POST">
+                                    @csrf @method("delete")
+                                    <button type="submit" class="btn btn-outline-danger btn-xs btn-accion-tabla eliminar tooltipsC" title="Desactivar registro">
+                                        <i class="fa fa-trash"></i>
                                     </button>
-                                </form>
+                                    </form>
+
+                                @else
+                                    <form action="{{route('page.desactivar.Rol', ['id' => $rs->id])}}" class="d-inline form-eliminar" method="POST">
+                                    @csrf @method("delete")
+                                    <button type="submit" class="btn btn-outline-success btn-xs btn-accion-tabla eliminar tooltipsC" title="Activar registro">
+                                        <i class="fa fa-recycle"></i>
+                                        </button>
+                                    </form>
+                                @endif
+                            @else
+                                <button class="btn btn-danger btn-sm  tooltipsC" title="Sin Acción">
+                                    <i class="fa fa-times-circle-o"> Sin Acciones </i>
+                                </button>
                             @endif
                         </td>
                     </tr>
